ce91a6432b15cfc1d25cec2047ddfbd99535408b
Kyle <accounts@klaynation.com>
2023-04-07 15:36:17 -0400

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -11,10 +11,10 @@ sudo apt install kbd libdrm-common libdrm2 python3-software-properties software-
 sudo apt autoremove -y
 
 # I didn't do any of this
-sudo apt install linux-lowlatency-hwe-22.04 fdutils ebtables -y
-sudo update-alternatives --config iptables
-sudo update-alternatives --config ebtables
-sudo apt remove linux-generic -y 
+# sudo apt install linux-lowlatency-hwe-22.04 fdutils ebtables -y
+# sudo update-alternatives --config iptables
+# sudo update-alternatives --config ebtables
+# sudo apt remove linux-generic -y 
 
 sudo apt autoremove -y
 sudo reboot
